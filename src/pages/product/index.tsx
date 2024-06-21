38fc77759d2a9c7c66e2e441d4560dd77b1b7435
Harun Ar - Rasyid <Harun.arrasyid1804@gmail.com>
2024-06-21 20:10:45 +0700

    Client side rendering

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,30 +1,50 @@
 import {useEffect, useState} from "react";
-import axios from "axios";
+import styles from "./Product.module.scss"
+import fetcher from "@/config/swr";
+import useSWR from "swr";
 
 type Product = {
     id: number;
     name: string;
     price: number;
-    size: string;
+    category: string;
+    image: string;
 }
 
 export default function ProductPage () {
-    const [products, setProducts] = useState([])
-    useEffect(() => {
-        axios.get("/api/products")
-            .then((response) => {
-                setProducts(response.data.data)
-            })
-            .catch((error) => {
-                console.error(error)
-            })
-    }, [])
+    const { data, error, isLoading } = useSWR("/api/products",fetcher);
+
     return (
-        <div>
-            <div>Product Page</div>
-            {products.map((product: Product) => (
-                <div key={product.id}>{product.name}</div>
-            ))}
+        <div className={styles.product}>
+            <h1 className={styles.product_title}>Product</h1>
+            <div className={styles.product_content}>
+                {!isLoading ? (
+                    <>
+                        {data.data.map((product: Product) => (
+                            <div key={product.id} className={styles.product_content_item}>
+                                <div className={styles.product_content_item_image}>
+                                    <img src={product.image} alt={product.name}/>
+                                </div>
+                                <h4 className={styles.product_content_item_name}>{product.name}</h4>
+                                <div className={styles.product_content_item_category}>{product.category}</div>
+                                <div className={styles.product_content_item_price}>
+                                    {product.price.toLocaleString("id-ID", {
+                                        style: "currency",
+                                        currency: "IDR",
+                                    })}
+                                </div>
+                            </div>
+                        ))}
+                    </>
+                ) : (
+                    <div className={styles.product_content_skeleton}>
+                        <div className={styles.product_content_skeleton_image}/>
+                        <div className={styles.product_content_skeleton_name}/>
+                        <div className={styles.product_content_skeleton_category}/>
+                        <div className={styles.product_content_skeleton_price}/>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
